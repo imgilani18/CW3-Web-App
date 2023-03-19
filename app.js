f832a21b7a578426ac02aa2460efd17a49c1b676
Owais Khan <51928444+owaiskhan3142@users.noreply.github.com>
2023-03-19 16:31:14 -0700

    hehe

---
+++ app.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import apiRouter from './api.js'
+import { client } from './db.js'
+import cors from 'cors';
+
+const app = express()
+const port = 3000
+
+app.use(express.json())
+app.use(cors());
+
+// Logger Middleware
+app.use((req, res, next) => {
+  res.on('finish', () => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url} ${res.statusCode}`)
+  })
+  next()
+})
+
+// Frontend path
+// app.use(express.static('vue.js'))
+
+// API routes
+app.use('/api', apiRouter)
+
+client.connect()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}!`)
+    })
+  })
--- src/util.js
+++
@@ -1,5 +0,0 @@
-const API_URL = 'http://3.15.148.102'
-
-export function fetchApi(path, data) { 
-  return fetch(API_URL + path, data); 
-}
