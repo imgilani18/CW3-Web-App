f0ea26227d4e3eb50fd404c21dae6954cbaf312e
Owais Khan <51928444+owaiskhan3142@users.noreply.github.com>
2023-03-22 14:21:06 -0700

    init

--- README.md
+++
@@ -1,5 +0,0 @@
-# CW3-Web-App
-
-https://github.com/imgilani18/CW3-Web-App.git
-
-https://imgilani18.github.io/CW3-Web-App/
---
+++ client/src/App.vue
@@ -0,0 +1,78 @@
+<script setup>
+import Lesson from './components/Lesson.vue'
+import Checkout from './components/Checkout.vue'
+import Options from './components/Options.vue'
+
+</script>
+
+<script>
+import { fetchApi } from './util'
+
+export default {
+  data() {
+    return {
+      searchTerm: '',
+      state: {
+        items: [],
+        cart: []
+      },
+      page: 'lesson'
+    }
+  },
+  methods: {
+    search(term) {
+      fetchApi(`/api/lesson?search=${term || ''}`)
+        .then(res => res.json())
+        .then((items) => {
+          items.forEach(item => {
+            item.availability = item.space
+          })
+
+          this.state.items = items
+          console.log(this.state)
+        })
+    },
+    addItem: function(item) {
+      item.availability--
+    },
+    removeItem: function(item) {
+      item.availability++
+    },
+  },
+  computed: {
+    cart() {
+      const cart = this.state.items.filter(item => item.availability < item.space)
+      console.log(cart)
+      return cart || []
+    },
+  },
+  beforeMount() {
+    this.search('')
+  },
+}
+</script>
+
+<template>
+  <div id="navbar">
+    <h2 >
+      <router-link class="link" to="/">Home</router-link>
+    </h2>
+    <div>
+      <div @click="page='cart'" v-if="cart.length" class="link">
+        <img class="icon" src="https://img.icons8.com/ios-glyphs/30/null/shopping-cart--v1.png"/>
+      {{ cart.map(i => i.space - i.availability).reduce((a,b) => a+b, 0)}}
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <div id="options">
+      <h1 class ='boxheading'>
+      </h1>
+      <input placeholder="Search" @input="search($event.target.value)" />
+      <Options :lessons="state.items" />
+
+    </div>
+  <Lesson :lessons="state.items" :additem="addItem" v-if="page=='lesson'"/>
+  <Checkout :cart="cart" :removeItem="removeItem" v-if="page=='cart'"/>
+  </div>
+</template>
