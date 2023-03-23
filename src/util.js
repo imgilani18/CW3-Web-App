091f52f4cc132c12b8e3741a5642e9f627699a3d
Owais Khan <51928444+owaiskhan3142@users.noreply.github.com>
2023-03-23 10:39:30 -0700

    Update util.js

--- src/util.js
+++ src/util.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://18.218.126.168:3000'
+const API_URL = 'http://3.133.154.7:3000'
 export function fetchApi(path, data) { 
   console.log("hehehehetalhaaaaaaaae");
   return fetch(API_URL + path, data); 
